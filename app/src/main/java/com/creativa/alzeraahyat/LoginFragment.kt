d4322c58431246ed47dbe885f8b10aa024e028e5
AhmedShehatah <ahmed2002.eg@gmail.com>
2022-02-14 11:42:13 +0200

    Initial commit

---
+++ app/src/main/java/com/creativa/alzeraahyat/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.creativa.alzeraahyat
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.creativa.alzeraahyat.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentLoginBinding
+        get() = FragmentLoginBinding::inflate
+    private lateinit var navController: NavController
+    override fun setupOnViewCreated(view: View) {
+        navController = Navigation.findNavController(view)
+        binding.btnLogin.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_fragmentTypesOfCourses)
+        }
+    }
+}
