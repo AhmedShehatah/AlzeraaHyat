213cd79d14414ea0e17ba2c7813e13678fc39294
AhmedShehatah <ahmed2002.eg@gmail.com>
2022-02-14 13:49:37 +0200

    types of Screen #3

---
+++ app/src/main/java/com/creativa/alzeraahyat/TypesOfCoursesAdapter.kt
@@ -0,0 +1,35 @@
+package com.creativa.alzeraahyat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.creativa.alzeraahyat.databinding.TypesModelBinding
+
+class TypesOfCoursesAdapter(private val list: ArrayList<String>) :
+    RecyclerView.Adapter<TypesOfCoursesAdapter.ViewHolder>() {
+    inner class ViewHolder(binding: TypesModelBinding) : RecyclerView.ViewHolder(binding.root) {
+        val text = binding.textView
+        val root = binding.root
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            TypesModelBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = list[position]
+        holder.text.text = item
+        holder.root.setOnClickListener {
+            
+        }
+
+    }
+
+    override fun getItemCount(): Int = list.size
+}
